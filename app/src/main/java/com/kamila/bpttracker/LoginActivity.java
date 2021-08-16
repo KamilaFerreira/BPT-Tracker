9a1bb17abfc511fed3ccfb0f2a890668cab22a6a
kamilaFerreira <kamilatferreira@gmail.com>
2021-08-16 18:34:53 +0100

    second commit
    added firebase to the project

---
+++ app/src/main/java/com/kamila/bpttracker/LoginActivity.java
@@ -0,0 +1,114 @@
+package com.kamila.bpttracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.kamila.bpttracker.config.ConfigFirebase;
+import com.kamila.bpttracker.model.User;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText edtEmail;
+    EditText edtPassword;
+    User user;
+    FirebaseAuth authentication;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String textEmail = edtEmail.getText().toString();
+                String textPassword = edtPassword.getText().toString();
+                //here the code will validate if the User fill up all the fields, case the fields are not filled, the app will display a message
+                if (!textEmail.isEmpty()) {
+                    if (!textPassword.isEmpty()) {
+
+                        user = new User(textEmail, textPassword);
+
+                        validateLogin();
+                        //this piece of code will validate the existing user
+
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Please, fill up the Password", //Toast displays a message
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please, fill up the E-mail", //Toast displays a message
+                            Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+
+    }
+
+    //this method will validate the existing User
+    public void validateLogin() {
+
+        authentication = ConfigFirebase.getFirebaseAuthentication();
+        authentication.signInWithEmailAndPassword(
+                user.getEmail(),
+                user.getPassword()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    openMainScreen();
+
+                } else {
+                    String exception = "";
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        exception = "User does not exist";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        exception = "Email and Password does not exist";
+                    } catch (Exception e) {
+                        exception = "Error to Register User" + e.getMessage();
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this, exception, // the Toast displyas the error message
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+    }
+
+    //this method Opens the Main Screen
+
+    private void openMainScreen() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish(); //this method closes the Login Activity
+
+    }
+}
+
+
--- app/src/main/java/com/kamila/bpttracker/MainActivity.java
+++ app/src/main/java/com/kamila/bpttracker/MainActivity.java
@@ -1,8 +1,16 @@
 package com.kamila.bpttracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("kamilatferrira@gmail.com").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document : task.getResult()){
+                        System.out.println("firestore" + document.getId());
+                        System.out.println("firestpore" + document.getData());
+                    }
+                }else{
+                    System.out.println("error" + task.getException());
+                }
+
+            }
+        });
+
+
     }
 }
