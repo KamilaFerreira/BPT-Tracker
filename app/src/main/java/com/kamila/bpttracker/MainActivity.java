bfc73aed71b8ffe4b8b54ac0dfcb1b5fc6e00bd5
kamilaFerreira <kamilatferreira@gmail.com>
2021-08-16 19:54:22 +0100

    third commit
    get collections

--- app/src/main/java/com/kamila/bpttracker/MainActivity.java
+++ app/src/main/java/com/kamila/bpttracker/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -12,32 +13,71 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    Double tempWeek;
+    Double tempDaily;
+    int countWeek;
+    int countDaily;
+    TextView tvBtWeek;
+    TextView tvBtDaily;
+    String currentDate;
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvBtWeek = (TextView) findViewById(R.id.tvBtWeek);
+        tvBtDaily = (TextView) findViewById(R.id.tvBtDaily);
+
+        tempWeek = 0.0;
+        tempDaily = 0.0;
+
+        countDaily = 0;
+        countWeek = 0;
+
+
         FirebaseApp.initializeApp(this);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        db.collection("kamilatferrira@gmail.com").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        db.collection("kamilatferreira@gmail.com").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
-                    for(QueryDocumentSnapshot document : task.getResult()){
-                        System.out.println("firestore" + document.getId());
-                        System.out.println("firestpore" + document.getData());
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        System.out.println("KKKKKKKKKKKKK" + document.getId());
+                        System.out.println("firestore" + document.getData());
+
+
+                        String docDate = (String) document.get("date");
+                        currentDate = df.format(new Date());
+                        if (currentDate.equals(docDate)) {
+
+                            countDaily++;
+                            tempDaily = tempDaily + Double.parseDouble((String) document.get("temperature"));
+
+                        }
+
+                        tempWeek = tempWeek + Double.parseDouble((String) document.get("temperature"));
+                        countWeek++;
                     }
-                }else{
+                    tvBtWeek.setText(String.valueOf(tempWeek / countWeek));
+                    tvBtDaily.setText(String.valueOf(tempDaily / countDaily));
+                } else {
                     System.out.println("error" + task.getException());
                 }
 
             }
         });
-
 
     }
 }
